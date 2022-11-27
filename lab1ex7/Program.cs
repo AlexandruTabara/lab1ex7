e37a9228c1effdda207fe37ebc6bf19fd50541c1
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-11-27 15:44:23 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex7
+            Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar
+                 In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+                 Google: even and odd number */
+
+            Console.WriteLine("Intordu numarul: ");
+
+            int i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Numarul introdus este par");
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus este impar");
+            }
+        }
+    }
+}
